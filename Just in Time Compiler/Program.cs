e4b49f0e177b33136ff823b1c13b24b5cb7604a2
Kulpot <kulpotwahu@gmail.com>
2023-09-23 02:50:00 +0800

    CS -> CSC(BUILD|ctrl+shift+B|) -> MSIL(MSintermediateLanguage) -> JIT(run(f5)) -> NativeLanguage(CPU)

--- Just in Time Compiler/Program.cs
+++ Just in Time Compiler/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 
 //ref link:https://www.youtube.com/watch?v=qkwrXQt6Oi0&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=9
-//  JIT(Just in Time) -> 
+//  JIT(Just in Time) -> compiles method by method, after MSIL when run(f5)
+// CS -> CSC(BUILD|ctrl+shift+B|) -> MSIL(MSintermediateLanguage) -> JIT(run(f5)) -> NativeLanguage(CPU)
 
 
 class AnotherClass
@@ -16,7 +17,7 @@ class MainClass
 {
     static void Main()
     {
-        AnotherClass.Moo();
+        AnotherClass.Moo(); //runtime - JIT
         AnotherClass.Moo();
     }
 }
